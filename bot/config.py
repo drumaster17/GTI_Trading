adec4bbe6b6029cf7a2b96120c78fe7ed7b0ec33
C.Eric <48843304+Belfor-IT@users.noreply.github.com>
2024-03-17 15:32:42 +0100

    aangepast

---
+++ bot/config.py
@@ -0,0 +1,16 @@
+# config.py
+EXCHANGES = {
+    "binance": {
+        "api_key": "YOUR_BINANCE_API_KEY",
+        "secret": "YOUR_BINANCE_SECRET",
+    },
+    "kucoin": {
+        "api_key": "YOUR_KUCOIN_API_KEY",
+        "secret": "YOUR_KUCOIN_SECRET",
+        "password": "YOUR_KUCOIN_PASSWORD", # Voor exchanges die dit vereisen
+    },
+    "coinbase": {
+        "api_key": "YOUR_COINBASE_API_KEY",
+        "secret": "YOUR_COINBASE_SECRET",
+    }
+}
